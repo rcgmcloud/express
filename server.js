cbf7bfff98c3b52626c7e385114448cfb99be7a0
Raymond Mariano <rcm@hawaii.edu>
2015-07-11 20:03:48 -1000

    added passport strategy

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ var config = require('./config/config.json');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    var admin = config.admin;
+    if (!admin) {
+      return done(new Error('No admin configured'));
+    }
+
+    if (username !== admin.username) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (password !== admin.password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, admin);
+
+  }
+));
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -23,26 +43,39 @@ app.use(session({
   }
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(function (req, res, next){
   var session = req.session;
   if(session.views){
     session.views++;
   }else{
-    session.views=1;
+    session.views = 1;
   }
-  //console.log('viewed', session.views, 'times!');
+  console.log('viewed', session.views, 'times!');
   next();
 });
+
+
 
 app.get('/', function (req, res) {
   res.render('index', {username: req.session.username});
 });
 
 
-app.post('/',function (req, res, next){
-  console.log('im doing something');
+app.post('/signup',function (req, res, next){
   req.session.username = req.body.name;
   res.redirect('/');
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/secret',
+                                    failureRedirect: '/',
+                                    session: false }));
+
+app.get('/secret', function(req, res, next) {
+  res.send('SECRET!');
 });
 
 var server = app.listen(3000, function () {
